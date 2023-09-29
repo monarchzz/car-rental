8689d0beeaaafa0361b7b6b479f3ff3ba8395dfa
TrungHieu <trunghieunguyendn@gmail.com>
2023-09-29 15:19:53 +0700

    update fcm

--- Car-Rental-Web-Admin/src/firebase-messaging-sw.js
+++ Car-Rental-Web-Admin/src/firebase-messaging-sw.js
@@ -1,14 +1,18 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging-compat.js');
+importScripts(
+    "https://www.gstatic.com/firebasejs/9.0.1/firebase-app-compat.js"
+);
+importScripts(
+    "https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging-compat.js"
+);
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDYXbOmPpcvD-tTFqRZuDvt75nvjHou1EI",
-    authDomain: "car-rental-236aa.firebaseapp.com",
-    projectId: "car-rental-236aa",
-    storageBucket: "car-rental-236aa.appspot.com",
-    messagingSenderId: "805399031928",
-    appId: "1:805399031928:web:308457cd9b7b8ea441fd8d",
-    measurementId: "G-2GZT37VSQL"
+    apiKey: "AIzaSyBSMHtxwcg5WBIlJIxPXo8PN2sZ793rLwg",
+    authDomain: "car-rental-2e561.firebaseapp.com",
+    projectId: "car-rental-2e561",
+    storageBucket: "car-rental-2e561.appspot.com",
+    messagingSenderId: "1089171419911",
+    appId: "1:1089171419911:web:dfd4f7d6e21c3d5c6e8ba2",
+    measurementId: "G-WBM1E5GV9Y",
 });
 
 const messaging = firebase.messaging();
@@ -18,17 +22,26 @@ messaging.onBackgroundMessage((payload) => {
         const notificationTitle = payload.notification.title;
         const notificationOptions = {
             body: payload.notification.body,
-            icon: '/assets/icons/icon-72x72.png',
+            icon: "/assets/icons/icon-72x72.png",
             renotify: false,
-            timestamp: Date.now()
+            timestamp: Date.now(),
         };
 
         // Kiểm tra xem có thông báo nào đang được hiển thị hay không
-        if (notifications.some(notification => notification.title === notificationTitle && notification.body === notificationOptions.body)) {
+        if (
+            notifications.some(
+                (notification) =>
+                    notification.title === notificationTitle &&
+                    notification.body === notificationOptions.body
+            )
+        ) {
             return;
         }
 
         // Hiển thị thông báo nếu không có thông báo nào đang được hiển thị
-        self.registration.showNotification(notificationTitle, notificationOptions);
+        self.registration.showNotification(
+            notificationTitle,
+            notificationOptions
+        );
     });
 });
