c0e2aaf734526ac226daea33391fb7025a596133
Trung Hieu <trunghieunguyendn@gmail.com>
2023-10-18 15:14:18 +0700

    create manager update

--- SourceCode/Car-Rental-Web-Admin/src/app/app-messages.ts
+++ SourceCode/Car-Rental-Web-Admin/src/app/app-messages.ts
@@ -5,6 +5,11 @@ const AppMessages = {
     updateError: 'Đã có lỗi xảy ra',
     occurError: 'Đã có lỗi xảy ra',
     usernameOrPhoneExist: 'Tên đăng nhập hoặc số điện thoại đã được sử dụng',
+    usernameMessage:
+        'Tài khoản phải ít hơn 20 ký tự và không chứa khoảng trắng.',
+    passwordMessage: 'Mật khẩu phải trên 8 ký tự chứa chữ in hoa và số.',
+    confirmPasswordMessage: 'Mật khẩu không khớp.',
+    phoneMessage: 'Vui lòng nhập số điện thoại đúng định dạng.',
 };
 
 export default AppMessages;
--- SourceCode/Car-Rental-Web-Admin/src/app/modules/admin/manager/create/create-manager.component.ts
+++ SourceCode/Car-Rental-Web-Admin/src/app/modules/admin/manager/create/create-manager.component.ts
@@ -1,23 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    Validators,
+} from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { confirmPasswordValidator, passwordValidator, phoneValidator, usernameValidator } from '@fuse/validators/custom-validator';
+import {
+    confirmPasswordValidator,
+    passwordValidator,
+    phoneValidator,
+    usernameValidator,
+} from '@fuse/validators/custom-validator';
 import { ManagerService } from '../manager.service';
+import AppMessages from 'app/app-messages';
 
 @Component({
     selector: 'app-create-manager',
-    templateUrl: 'create-manager.component.html'
+    templateUrl: 'create-manager.component.html',
 })
-
 export class CreateManagerComponent implements OnInit {
-
     createManagerForm: UntypedFormGroup;
+    usernameMessage = AppMessages.usernameMessage;
+    passwordMessage = AppMessages.passwordMessage;
+    confirmPasswordMessage = AppMessages.confirmPasswordMessage;
+    phoneMessage = AppMessages.phoneMessage;
 
     constructor(
         private _managerServive: ManagerService,
         public matDialogRef: MatDialogRef<CreateManagerComponent>,
         private _formBuilder: UntypedFormBuilder
-    ) { }
+    ) {}
 
     ngOnInit() {
         this.initCreateManagerForm();
@@ -32,10 +44,14 @@ export class CreateManagerComponent implements OnInit {
             gender: ['', Validators.required],
             phone: ['', [Validators.required, phoneValidator()]],
         });
-        this.createManagerForm.get('confirmPassword').setValidators([
-            Validators.required,
-            confirmPasswordValidator(this.createManagerForm.get('password'))
-        ]);
+        this.createManagerForm
+            .get('confirmPassword')
+            .setValidators([
+                Validators.required,
+                confirmPasswordValidator(
+                    this.createManagerForm.get('password')
+                ),
+            ]);
     }
 
     public onGenderChange(value: string) {
@@ -44,19 +60,24 @@ export class CreateManagerComponent implements OnInit {
 
     public createManager() {
         if (this.createManagerForm.valid) {
-            this._managerServive.createManager(this.createManagerForm.value).subscribe(manager => {
-                if (manager) {
-                    this.matDialogRef.close('success');
-                } else {
-                    this.matDialogRef.close('error');
-                }
-            }, error => {
-                console.log(error.status);
+            this._managerServive
+                .createManager(this.createManagerForm.value)
+                .subscribe(
+                    (manager) => {
+                        if (manager) {
+                            this.matDialogRef.close('success');
+                        } else {
+                            this.matDialogRef.close('error');
+                        }
+                    },
+                    (error) => {
+                        console.log(error.status);
 
-                if (error.status === 400) {
-                    this.matDialogRef.close('error_duplicate');
-                }
-            })
+                        if (error.status === 400) {
+                            this.matDialogRef.close('error_duplicate');
+                        }
+                    }
+                );
         }
     }
 }
